6cdf1aac71da8133690f7fe824b6dad26173626a
jinyeopoh <johh0010@student.monash.edu>
2023-03-16 22:12:39 +1100

    PageNotFound page is created. API call to get weather data implemented a bit. This is on going

--- fyp/src/app/components/chart/chart.component.ts
+++ fyp/src/app/components/chart/chart.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { IWeather } from 'src/app/global/interfaces';
+
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent {
 
+export class ChartComponent implements OnInit{
+
+  weatherData: IWeather | null = null; 
+
+  constructor(private apiService: ApiService){}
+
+  /**
+   * This method is triggered upon loading the page.
+   * 
+   * It makes API call to retrieve weather data.
+   * getWeatherData() returns Observable object, so it must be subscribed to be executed.
+   */
+  ngOnInit(): void {
+    this.apiService.getWeatherData().subscribe(data => {
+      this.weatherData = data as IWeather
+      console.log(this.weatherData);
+      console.log(this.weatherData.dateGenerated);
+      console.log(this.weatherData.data);
+    })
+  }
 }
---
+++ fyp/src/app/global/constants.ts
@@ -0,0 +1,7 @@
+// Angular Modules
+import { Injectable } from '@angular/core'; 
+
+@Injectable()
+export class Constants {
+    public static readonly API_WEATHER_ENDPOINT: string = "https://api.meteomatics.com/"; 
+}
