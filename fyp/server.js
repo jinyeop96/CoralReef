89a4f33ecd44dfd3d0bc1415d774de3eeb6e045b
jinyeopoh <jinyeopoh96@gmail.com>
2023-04-02 19:36:44 +1000

    server.js conflict resolved

--- fyp/server.js
+++ fyp/server.js
@@ -1,37 +1,48 @@
 const express = require("express");
 const { urlencoded } = require("body-parser");
 const path = require('path')
-
-// Routers
-const testRouter = require("./routers/testRouter")
-
-
+// const mysql = require('mysql');
+var debug = require('debug')('web:server');
+var http = require('http');
+const mysqlRouter = require('./web/login');
 const app = express();
-app.listen(8080)
-
+//allow cross-domain
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use('/', express.static(path.join(__dirname, 'dist/fyp')))
 app.use(express.json());
 app.use(urlencoded({extended:true}));
-
-// RESTful APIs for MySql database
-app.post('/postTest', testRouter.postTest)
-app.get('/getTest', testRouter.getTest)
+app.use("/login",mysqlRouter);
 
 
-// app.post('/test', (req, res) => {
-//     let newLocation = req.body
 
-//     let sql = "INSERT INTO testLocation SET ?"
-//     connection.query(sql, newLocation, function (err, result, fields) {
-//         if (err) throw err;
-//         console.log(result);
-//     });
+
+connection.connect( err => {
+    if (err) {
+        throw err;
+    }
+
+    console.log("connection done");
+})
+
+app.post('/test', (req, res) => {
+    let newLocation = req.body
+
+    let sql = "INSERT INTO testLocation SET ?"
+    connection.query(sql, newLocation, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
     // connection.query("INSERT INTO testLocation (location, state) values (`${location}`, `${state}`)", function (err, result, fields) {
     //     if (err) throw err;
     //     console.log(result);
     // });
     
-// })
+})
 
 // connection.query('insert into testing(name) values ("jinyeop")', (err, rows, fields) => {
 //     if (err) throw err
@@ -40,17 +51,118 @@ app.get('/getTest', testRouter.getTest)
 // })
   
 
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-// const url = "mongodb://localhost:27017/theatre";
-// mongoose.connect(url, function(err, result){
-//     if(err){
-//         console.log(err);
-//     } else {
-//         console.log("successful");
-//     }
-// });
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+/**
+ * Module dependencies.
+ */
+
+/**
+ * Get port from environment and store in Express.
+ */
 
 
-// app.get("/", router.);
-// app.get("/test", router);
 
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+var port = normalizePort("8080");
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
--- fyp/src/app/components/login/login.component.ts
+++ fyp/src/app/components/login/login.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface User{
+  user_name:string,
+  user_pwd:string,
+  confirmPassword:string,
+  name:string,
+  remark:string
+}
 
 @Component({
   selector: 'app-login',
@@ -6,5 +15,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user:User={
+    user_name:"",
+    user_pwd:"",
+    confirmPassword:"",
+    name:"",
+    remark:""
+  }
+  constructor(private http: HttpClient) { }
 
+  login() {
+
+   this.http.post('http://localhost:8080/login/login',this.user).subscribe((response:any) => {
+    if(response.message=="success"){
+      console.log("登录成功");
+
+    }else{
+      console.log("登录失败");
+
+    }
+  });
+  }
+  register(){
+    if(this.user.confirmPassword==this.user.user_pwd){
+    var obj={
+      user_name:"",
+      user_pwd:"",
+      name:"",
+      remark:""
+    }
+    obj.user_name=this.user.user_name
+    obj.user_pwd=this.user.user_pwd
+    obj.name=this.user.name
+    obj.remark=this.user.remark
+
+   this.http.post('http://localhost:8080/login/register',obj).subscribe((response:any) => {
+    if(response.message=="success"){
+      console.log("注册成功");
+
+    }else{
+      console.log("注册失败");
+
+    }
+  });
+  return;
+}
+console.log("确认密码和密码不匹配");
+
+  }
 }
