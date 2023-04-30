3a8ec973ff7f242e229237b9dbbaa7ea64d6a0d2
jinyeopoh <jinyeopoh96@gmail.com>
2023-04-30 16:32:06 +1000

    Main page completed

--- fyp/src/app/app.component.ts
+++ fyp/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'fyp';
+
+  constructor() {}
+
+  onActivate(event: any): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })    
+  }
 }
