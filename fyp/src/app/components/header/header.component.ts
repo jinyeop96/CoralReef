154ee497c9eac19a99d07a14c2146f120581a615
jinyeopoh <jinyeopoh96@gmail.com>
2023-05-11 10:15:56 +1000

    UI changed

--- fyp/src/app/components/header/header.component.ts
+++ fyp/src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent{
+
+  title: string = ""
+  
+  constructor(private route: ActivatedRoute) {}
+
+  // ngOnInit(): void {
+  //   this.route.queryParamMap.subscribe( params => {
+  //     console.log(params.get('title'))
+  //   })
+  // }
+
 
 }
