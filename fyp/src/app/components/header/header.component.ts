f2a83d65a5dad401769f8c090de9f267ccb09a6c
jinyeopoh <jinyeopoh96@gmail.com>
2023-05-11 10:40:40 +1000

    UI modified to make it consistent

--- fyp/src/app/components/header/header.component.ts
+++ fyp/src/app/components/header/header.component.ts
@@ -8,13 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeaderComponent{
 
-  title: string = ""
+  title: string | any = ""
+  subTitle = "We provide information about coral reef ecosystem"
+  showSubTitle: boolean = true;
   
   constructor(private route: ActivatedRoute) {}
 
   // ngOnInit(): void {
   //   this.route.queryParamMap.subscribe( params => {
-  //     console.log(params.get('title'))
+  //     const paramTitle = params.get('title');
+
+  //     if( paramTitle == null || paramTitle == undefined) {
+  //       this.title = "Welcome to Coral Reef"
+  //       this.showSubTitle = false;
+  //     } else if (paramTitle.toUpperCase() == 'HOME'){
+  //       this.title = "Welcome to Coral Reef"
+  //       this.showSubTitle = true;
+  //     } else {
+  //       this.title = paramTitle
+  //       this.showSubTitle = false;
+  //     }
   //   })
   // }
 
